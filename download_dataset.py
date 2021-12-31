e0ce5ee5c22e5eaa6db22b4440cf2ac329023fbf
Robin CUGNY <rcugny@solutiondatagroup.fr>
2021-12-31 09:49:23 +0100

    Add pima message in dl_dataset

--- download_dataset.py
+++ download_dataset.py
@@ -11,10 +11,15 @@ def download_dataset(name):
         y = diabetes.target
         feature_names = diabetes.feature_names
 
-        df_X = pd.DataFrame(X,columns=diabetes.feature_names)
+        df_X = pd.DataFrame(X,columns=feature_names)
         df_y = pd.Series(y,name='diabete progression')
         df_X["measure of disease progression"] = df_y
         df_X.to_csv("data/diabetes.csv",index=False)
+    elif name=="pima indians":
+        print("Go to https://www.kaggle.com/uciml/pima-indians-diabetes-database")
+        print("Join the competition if necessary, download the dataset, extract it and copy it in the data/ folder.")
+    else:
+        raise ValueError("This dataset is not implemented yet.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
